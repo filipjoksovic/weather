e744eb57e33789911f4d81d7f23defb0cf86c8dc
Filip Joksovic <filipjoksovic1@gmail.com>
2024-02-14 00:30:58 +0100

    feat: Initial layout (#2)
    * feat: introduce default layout component
    
    * feat: introduce default layout component
    
    * feat: rough default layout implementation
    
    * feat: default layout improvements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './layout/components/default-layout/default-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: DefaultLayoutComponent }];
---
+++ src/app/layout/components/current-measurement/current-measurement.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-measurement',
+  standalone: true,
+  imports: [],
+  templateUrl: './current-measurement.component.html',
+})
+export class CurrentMeasurementComponent {}

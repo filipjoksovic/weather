7bd62ea14cf97c938d89d8642c1a7e330af07959
Filip Joksovic <filipjoksovic1@gmail.com>
2024-02-13 21:56:55 +0100

    chore: introduce eslint and prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,16 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {Spectator} from "@ngneat/spectator";
-import {createComponentFactory} from "@ngneat/spectator/jest";
+import { Spectator } from '@ngneat/spectator';
+import { createComponentFactory } from '@ngneat/spectator/jest';
 
 describe('AppComponent', () => {
   let spectator: Spectator<AppComponent>;
   const createComponent = createComponentFactory(AppComponent);
 
-  beforeEach(()=>{
+  beforeEach(() => {
     spectator = createComponent();
-  })
+  });
 
-  it("Should render component",()=>{
+  it('Should render component', () => {
     expect(spectator).toBeDefined();
-  })
+  });
 });

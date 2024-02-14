ad20849509be218ce3c2f487f1485e8b75dd68d9
Filip Joksovic <filipjoksovic1@gmail.com>
2024-02-14 17:07:00 +0100

    feat, test: current weather display improvements

--- src/app/components/current-measurement/current-measurement.component.spec.ts
+++ src/app/components/current-measurement/current-measurement.component.spec.ts
@@ -1,22 +1,85 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CurrentMeasurementComponent } from './current-measurement.component';
+import {
+  byTestId,
+  createComponentFactory,
+  Spectator,
+} from '@ngneat/spectator/jest';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  LoadableData,
+  LoadbleDataLoading,
+  LoadingState,
+  WeatherService,
+} from '../../services/weather.service';
+import { of } from 'rxjs';
+import {
+  CurrentWeather,
+  currentWeatherResponseToCurrentWeather,
+} from '../../models/current-weather.model';
+import { CURRENT_RESPONSE } from '../../../assets/MOCKS/current-weather-response.mock';
 
 describe('CurrentMeasurementComponent', () => {
-  let component: CurrentMeasurementComponent;
-  let fixture: ComponentFixture<CurrentMeasurementComponent>;
+  let spectator: Spectator<CurrentMeasurementComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CurrentMeasurementComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(CurrentMeasurementComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  const createComponent = createComponentFactory({
+    component: CurrentMeasurementComponent,
+    detectChanges: false,
+    shallow: false,
+    imports: [HttpClientTestingModule],
+    providers: [],
+    declarations: [],
+    mocks: [WeatherService],
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    spectator = createComponent();
+  });
+
+  it('Should define component (smoke)', () => {
+    expect(spectator).toBeDefined();
+  });
+
+  it('Should display loading state when weather data is loading', () => {
+    spectator.setInput(
+      'currentWeatherState$',
+      of({
+        state: LoadingState.LOADING,
+      } as LoadbleDataLoading)
+    );
+    spectator.detectChanges();
+    const skeletonContainer = spectator.query(byTestId('loading-container'));
+    const skeletonElements = spectator.queryAll('app-skeleton');
+    expect(skeletonContainer).toExist();
+    expect(skeletonElements.length).toBe(4);
+  });
+
+  it('Should display data upon load when data has been loaded', () => {
+    spectator.setInput(
+      'currentWeatherState$',
+      of({
+        state: LoadingState.LOADED,
+        data: currentWeatherResponseToCurrentWeather(CURRENT_RESPONSE),
+      } as LoadableData<CurrentWeather>)
+    );
+    spectator.detectChanges();
+    spectator.detectComponentChanges();
+
+    const dataContainer = spectator.query(byTestId('data-container'));
+    const currentTemperature = spectator.query(byTestId('current-temperature'));
+    const currentDescription = spectator.query(byTestId('current-description'));
+    const currentMinimum = spectator.query(byTestId('current-minimum'));
+    const currentMaximum = spectator.query(byTestId('current-maximum'));
+    expect(dataContainer).toExist();
+    expect(currentDescription).toExist();
+    expect(currentTemperature).toExist();
+    expect(currentMinimum).toExist();
+    expect(currentMaximum).toExist();
+
+    expect(currentMaximum?.querySelector('fa-icon')).toExist();
+    expect(currentMaximum).toHaveText('1.82');
+    expect(currentMinimum?.querySelector('fa-icon')).toExist();
+    expect(currentMinimum).toHaveText('1.82');
+    expect(currentTemperature).toHaveText('1.82');
+    expect(currentDescription).toHaveText('clear sky');
   });
 });
--- src/app/components/current-measurement/current-measurement.component.ts
+++ src/app/components/current-measurement/current-measurement.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { LoadingState, WeatherService } from '../../services/weather.service';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { SkeletonComponent } from '../skeleton/skeleton.component';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-current-measurement',
   standalone: true,
-  imports: [NgIf, AsyncPipe, SkeletonComponent],
+  imports: [NgIf, AsyncPipe, SkeletonComponent, FaIconComponent],
   templateUrl: './current-measurement.component.html',
 })
 export class CurrentMeasurementComponent {
@@ -18,4 +20,6 @@ export class CurrentMeasurementComponent {
   }
 
   protected readonly LoadingState = LoadingState;
+  protected readonly faArrowUp = faArrowUp;
+  protected readonly faArrowDown = faArrowDown;
 }

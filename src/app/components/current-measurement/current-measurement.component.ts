ae46be57c17046ee70bc7212b9c6d956e5c01bc0
Filip Joksovic <filipjoksovic1@gmail.com>
2024-02-14 02:21:44 +0100

    feat: current weather display

--- src/app/components/current-measurement/current-measurement.component.ts
+++ src/app/components/current-measurement/current-measurement.component.ts
@@ -1,16 +1,21 @@
 import { Component, inject } from '@angular/core';
-import { WeatherService } from '../../../services/weather.service';
+import { LoadingState, WeatherService } from '../../services/weather.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { SkeletonComponent } from '../skeleton/skeleton.component';
 
 @Component({
   selector: 'app-current-measurement',
   standalone: true,
-  imports: [],
+  imports: [NgIf, AsyncPipe, SkeletonComponent],
   templateUrl: './current-measurement.component.html',
 })
 export class CurrentMeasurementComponent {
   private readonly weatherService: WeatherService = inject(WeatherService);
 
+  public currentWeatherState$ = this.weatherService.currentWeather$;
   constructor() {
     this.weatherService.getCurrentWeather();
   }
+
+  protected readonly LoadingState = LoadingState;
 }

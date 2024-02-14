c27ee23cfcb46f86ea6fb9b6c70db0d6fdecefe1
Filip Joksovic <filipjoksovic1@gmail.com>
2024-02-14 21:31:22 +0100

    feat: Display time when a last fetch occured in location-data component

--- src/app/components/location-data/location-data.component.ts
+++ src/app/components/location-data/location-data.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { combineLatestWith, filter, map, tap } from 'rxjs';
+import { LoadingState, WeatherService } from '../../services/weather.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { DateFormatPipe } from '../../pipes/date-format.pipe';
 
 @Component({
   selector: 'app-location-data',
   standalone: true,
-  imports: [],
+  imports: [NgIf, AsyncPipe, DateFormatPipe],
   templateUrl: './location-data.component.html',
   styleUrl: './location-data.component.scss',
 })
-export class LocationDataComponent {}
+export class LocationDataComponent {
+  private readonly weatherService: WeatherService = inject(WeatherService);
+
+  private readonly currentWeatherLoaded$ =
+    this.weatherService.currentWeather$.pipe(
+      filter(state => state.state === LoadingState.LOADED)
+    );
+
+  private readonly forecastLoaded$ = this.weatherService.forecast$.pipe(
+    filter(state => state.state === LoadingState.LOADED)
+  );
+
+  dataLoaded$ = this.currentWeatherLoaded$.pipe(
+    combineLatestWith(this.forecastLoaded$),
+    tap(console.log),
+    map(() => new Date())
+  );
+
+  reloadData() {}
+}

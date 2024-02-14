1724c6e44bf5b5f9535509583a6d279f8477b33c
Filip Joksovic <filipjoksovic1@gmail.com>
2024-02-14 23:09:31 +0100

    feat: Cache every request and display latest fetched value on initial load (#8)
    * feat: introduce a storage service for caching purposes
    
    * feat: display cached values on initial load

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  private readonly storage: Storage = localStorage;
+  constructor() {}
+
+  public set(key: string, value: string | object) {
+    try {
+      this.storage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(
+        `Error occured while attempting to store data in localStorage`,
+        error
+      );
+    }
+  }
+
+  public get<T>(key: string): T | null {
+    try {
+      const obj = this.storage.getItem(key);
+      if (!obj) {
+        return null;
+      } else {
+        return JSON.parse(obj) as T;
+      }
+    } catch (error) {
+      console.error(
+        `Error occured while attempting to fetch data from localStorage`,
+        error
+      );
+      return null;
+    }
+  }
+
+  public remove(key: string) {
+    try {
+      this.storage.removeItem(key);
+    } catch (error) {
+      console.error(
+        `Error occured while attempting to remove data from localStorage`,
+        error
+      );
+    }
+  }
+
+  public clear() {
+    try {
+      this.storage.clear();
+    } catch (error) {
+      console.error(
+        `Error occured while attempting to clear localStorage`,
+        error
+      );
+    }
+  }
+}
